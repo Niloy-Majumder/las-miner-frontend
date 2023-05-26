e34eba4c649358df2a63a2f283d4bd82fe6ec780
Niloy Majumder <niloymajumder.98@gmail.com>
2023-05-26 08:15:30 +0530

    changed contract address

--- src/web3/contractAddress.js
+++ src/web3/contractAddress.js
@@ -1,4 +1,5 @@
-const contractAddress = "0x060E3230343fb512e0d8C954e2bCD203baC3EA71";
+const contractAddress = "0x362b79c3e6b2450Edb16577f0EaB84350F24a66a";
+//"0x060E3230343fb512e0d8C954e2bCD203baC3EA71";
 /*"0x8c5f37B8BA05F1f78c66055eD564970F05e71b69";*/
 
 export default contractAddress;
